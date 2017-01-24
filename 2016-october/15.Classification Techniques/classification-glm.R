c694bf94696ca841870427b38767c6ad275c35c7
Algorithmica <info@algorithmica.co.in>
2017-01-24 09:52:05 -0600

    uploading logistic regression notes

--- 2016-october/15.Classification Techniques/classification-glm.R
+++ 2016-october/15.Classification Techniques/classification-glm.R
@@ -1,5 +1,6 @@
 library(rpart)
 library(caret)
+library(RWeka)
 
 setwd("C:/Users/Algorithmica/Downloads")
 titanic_train = read.csv("train.csv", na.strings = c("NA",""))
@@ -13,22 +14,17 @@ titanic_train$Pclass = as.factor(titanic_train$Pclass)
 #model building using formula interface
 #In formula interface, caret train function automatically transforms all factor types to continuous types
 set.seed(100)
-#bag_tree_grid = expand.grid(.cp=seq(0,0.2,0.001))
-resampling_strategy = trainControl(method="boot", number = 10)
+resampling_strategy = trainControl(method="cv", number = 10)
 
-bag_tree_model = train(titanic_train[,c("Sex", "Pclass", "Fare")], titanic_train$Survived, method="treebag", trControl = resampling_strategy)
-bag_tree_model$finalModel$mtrees[[2]]
-
-rf_grid = expand.grid(.mtry=c(2,3))
-rf_model = train(titanic_train[,c("Sex", "Pclass", "Parch", "SibSp")], titanic_train$Survived, method="rf", trControl = resampling_strategy, ntree=3000, tuneGrid = rf_grid)
-getTree(rf_model$finalModel, 1, labelVar = T)
+lr_model1 = train(Survived ~ Sex + Pclass, titanic_train, method="glm", trControl = resampling_strategy)
+lr_model1$finalModel
 
 titanic_test = read.csv("test.csv", na.strings = c("NA",""))
 dim(titanic_test)
 str(titanic_test)
 titanic_test$Pclass = as.factor(titanic_test$Pclass)
 
-titanic_test$Survived = predict(bag_tree_model1, titanic_test)
+titanic_test$Survived = predict(lr_model1, titanic_test)
 write.csv(titanic_test[,c("PassengerId","Survived")],"submission.csv", row.names = F)
 
 
