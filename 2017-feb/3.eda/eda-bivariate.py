7a87734abe9811164d1654aaa7b115316e5c9488
Algorithmica <info@algorithmica.co.in>
2017-02-23 10:01:32 -0600

    uploading eda code

--- 2017-feb/3.eda/eda-bivariate.py
+++ 2017-feb/3.eda/eda-bivariate.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import seaborn as sns
+import numpy as np
 
 #returns current working directory
 os.getcwd()
@@ -19,6 +21,33 @@ titanic_train['Embarked'] = titanic_train['Embarked'].astype('category')
 
 
 #explore bivariate relationships: categorical vs categorical 
-tmp1 = pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Sex'])
-type(tmp1)
+pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Sex'])
 pd.crosstab(index=titanic_train['Survived'], columns=titanic_train['Pclass'], margins=True)
+
+sns.factorplot(x="Survived", hue="Sex", data=titanic_train, kind="count", size=6)
+sns.factorplot(x="Pclass", hue="Survived", data=titanic_train, kind="count", size=6)
+sns.factorplot(x="Embarked", hue="Survived", data=titanic_train, kind="count", size=6)
+
+#explore bivariate relationships: categorical vs continuous 
+sns.factorplot(x="Fare", row="Survived", data=titanic_train, kind="box", size=6)
+
+sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.kdeplot, "Fare").add_legend()
+sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.distplot, "Fare").add_legend()
+sns.FacetGrid(titanic_train, row="Survived",size=8).map(sns.boxplot, "Fare").add_legend()
+
+#explore bivariate relationships: continuous vs continuous 
+np.cov(titanic_train['SibSp'], titanic_train['Parch'])
+np.corrcoef(titanic_train['SibSp'], titanic_train['Parch'])
+sns.jointplot(x="SibSp", y="Parch", data=titanic_train)
+sns.jointplot(x="SibSp", y="Parch", data=titanic_train, kind='kde')
+
+v1 = [10,12,15,20,22,25]
+v2 = [20,22,25,27,32,35]
+v3 = [35,32,30,29,27,26]
+np.cov(v1,v2)
+np.corrcoef(v1,v2)
+df = pd.DataFrame({'v1':v1,'v2':v2,
+'v3':v3})
+sns.jointplot(x="v1", y="v3", data=df)
+
+sns.pairplot(titanic_train)
--- 2017-feb/3.eda/standard-deviation.py
+++ 2017-feb/3.eda/standard-deviation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 ibm_stock = [10,12,15,15,10,13]
 ms_stock = [20,25,30,20,15,22]
@@ -10,5 +11,19 @@ stocks['ms_stock'].std()
 stocks['ibm_stock'].mad()
 stocks['ms_stock'].mad()
 
+np.cov(ibm_stock, ms_stock)
+np.corrcoef(ibm_stock, ms_stock)
 
+oracle_stock = [100,110,120,130,140,150]
+np.cov(ibm_stock, oracle_stock)
+np.corrcoef(ibm_stock, oracle_stock)
 
+google_stock = [200,210,220,230,240,250]
+np.cov(oracle_stock, google_stock)
+np.corrcoef(oracle_stock, google_stock)
+
+amazon_stock = [250,240,230,220,210,200]
+np.cov(google_stock, amazon_stock)
+np.corrcoef(google_stock, amazon_stock)
+
+np.corrcoef(google_stock, amazon_stock)
