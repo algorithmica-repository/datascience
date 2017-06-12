44c4e7c3fb6aa01abb179f4791712ed3d4f52716
Algorithmica <info@algorithmica.co.in>
2017-06-12 11:43:05 +0530

    uploading pending code

--- 2017-may/10.probability/random.py
+++ 2017-may/10.probability/random.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-coin_tosses = np.random.randint(0,2,10000)
+coin_tosses = np.random.randint(0,2,1000)
 pd.crosstab(index=coin_tosses, columns="count")
 
 dice_throws = np.random.randint(1,7,100000)
