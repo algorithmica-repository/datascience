83a9cd5a4e60f34c77335d8a6a874e169b05fdcc
Algorithmica <info@algorithmica.co.in>
2017-08-30 05:59:55 +0530

    uploading python code discussed in class

---
+++ 2017-aug/1.python/basic-containers.py
@@ -0,0 +1,13 @@
+#understanding basic types
+#types are inferred by runtime automatically
+
+a = 10
+print(type(a))
+a = 13.6
+print(type(a))
+a = 'abc'
+print(type(a))
+a = True
+print(type(a))
+
+print(a+10)
---
+++ 2017-aug/1.python/list.py
@@ -0,0 +1,14 @@
+list1 = [10,20,30]
+print(type(list1))
+
+list2 = ['abc', 10, True]
+
+#access list elements by slicing
+print(list1[0])
+print(list1[0:3])
+print(list1[2:3])
+
+list1[2] = 100
+print(list1)
+
+list1.append(10)
