43eec711407c0828dfc1f5f765c24ee466c4712e
Algorithmica <info@algorithmica.co.in>
2017-10-22 05:11:27 +0530

    uploading new assignments

--- 2017-aug/2.kaggle-classification-I/kaggle-II.py
+++ 2017-aug/2.kaggle-classification-I/kaggle-II.py
@@ -1,26 +1,26 @@
-import pandas as pd
-print(pd.__version__)
-titanic_train = pd.read_csv("D:/titanic/train.csv")
-print(type(titanic_train))
-
-#explore the dataframe
-titanic_train.shape
-titanic_train.info()
-
-#access columns of a dataframe
-titanic_train['Sex']
-titanic_train['Fare']
-titanic_train.Sex
-
-titanic_train['Survived']
-titanic_train.groupby(['Sex', 'Survived']).size()
-
-titanic_test = pd.read_csv('D:/titanic/test.csv')
-titanic_test.shape
-titanic_test.info()
-
-titanic_test.loc[titanic_test.Sex=='male','Survived'] = 0
-titanic_test.loc[titanic_test.Sex=='female','Survived'] = 1
-titanic_test.to_csv('D:/titanic/submission.csv', columns=['PassengerId','Survived'],index=False)
-
-
+                            nlp
+                          -------
+kishoreboddapaty@gmail.com,
+yenipeta.manjunath@gmail.com,
+meenakshi.mishra@gmail.com,
+santhosh.challa@gmail.com,
+telukishore@gmail.com,
+cnu.jilla@gmail.com,
+prynjl@gmail.com,
+madhubigscience@gmail.com,
+pranay.kewl@gmail.com,
+subhashkonda@gmail.com,
+ptrehun@gmail.com,
+sandeep.kshirsagar570@gmail.com,
+niki196@gmail.com,
+jaspreetsingh0707@gmail.com,
+ramayyanv37@gmail.com,
+srinuvasulu.purini@gmail.com,
+mukkahussainreddy@gmail.com,
+lakshya07khanna@gmail.com,
+sanjeev.vikkurthi@gmail.com,
+adariramesh3@gmail.com,
+prudvikrishna@gmail.com,
+maruthi.psn@gmail.com,
+sangipraveen@gmail.com,
+subbarao3642@gma
