7558af5760534cbf3cb8469254372c8a87c280eb
Algorithmica <info@algorithmica.co.in>
2018-05-29 14:12:49 +0530

    class code

--- 2018-mar/12.kaggle-regression/kaggle-I.py
+++ 2018-mar/12.kaggle-regression/kaggle-I.py
@@ -6,7 +6,7 @@ import numpy as np
 from sklearn import model_selection, ensemble
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.externals import joblib
+from sklearn import feature_selection
 
 class MyLabelBinarizer(preprocessing.LabelBinarizer):
     def transform(self, y):
@@ -51,17 +51,71 @@ def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
 
     plt.legend(loc="best")
 
-def plot_feature_importances(classifier, X_train, name):
+def plot_feature_importances(X_train, y_train):
+    classifier = ensemble.RandomForestRegressor(random_state=100, n_jobs=1, verbose=1, oob_score=True)
+    classifier.fit(X_train, y_train)
     indices = np.argsort(classifier.feature_importances_)[::-1][:40]
     g = sns.barplot(y=X_train.columns[indices][:40],x = classifier.feature_importances_[indices][:40] , orient='h')
     g.set_xlabel("Relative importance",fontsize=12)
     g.set_ylabel("Features",fontsize=12)
     g.tick_params(labelsize=9)
-    g.set_title(name + " feature importance")
+    g.set_title("RF feature importances")
+    return classifier
+      
+def map_cat_to_numerics(df, features, mappings):
+    for feature in features:
+        null_idx = df[feature].isnull()
+        df.loc[null_idx, feature] = None 
+        df[feature] = df[feature].map(mappings)
         
+def get_continuous_columns(df):
+    return df.select_dtypes(include=['number']).columns
+
+def get_categorical_columns(df):
+    return df.select_dtypes(exclude=['number']).columns
+
+def transform_cat_to_cont(df, cat_features, cont_features):
+    feature_defs = []
+    for col_name in cat_features:
+        feature_defs.append((col_name, MyLabelBinarizer()))
+    
+    for col_name in cont_features:
+        feature_defs.append((col_name, None))
+
+    mapper = DataFrameMapper(feature_defs, input_df=True, df_out=True)
+    mapper.fit(df)
+    return mapper.transform(df)
+
+def cast_cont_to_cat(df, features):
+    for feature in features:
+        df[feature] = df[feature].astype('category')
+
+def impute_categorical_features(df, features):
+    #impute missing values for categorical features
+    cat_imputer = CategoricalImputer()
+    cat_imputer.fit(df[features])
+    print(cat_imputer.fill_)
+    df[features] = cat_imputer.transform(df[features])
+
+def impute_continuous_features(df, features):
+    cont_imputer = preprocessing.Imputer()
+    cont_imputer.fit(df[features])
+    print(cont_imputer.statistics_)
+    df[features] = cont_imputer.transform(df[features])
+
+def get_features_to_drop(df, threshold) :
+    tmp = df.isnull().sum()
+    return list(tmp[tmp/float(df.shape[0]) > threshold].index)
+
+def drop_features(df, features):
+    return df.drop(features, axis=1, inplace=False)
+    
+def select_features(X_train, y_train):
+    return
+
+
 #changes working directory
 path = 'D:/'
-
 house_train = pd.read_csv(os.path.join(path,"house-train.csv"))
 house_train.shape
 house_train.info()
@@ -73,91 +127,83 @@ house_test.info()
 house_all = pd.concat([house_train, house_test])
 house_all.shape
 
-house_train = house_all[0:house_train.shape[0]].copy()
+#preprocess features
+features_to_cast = ['MSSubClass']
+cast_cont_to_cat(house_all, features_to_cast)
 
+house_train = house_all[0:house_train.shape[0]].copy()
+house_train.info()
+features_to_drop = get_features_to_drop(house_train, 0.0)
+features_to_drop.append('SalePrice')
+features_to_drop.append('Id')
+
+house_train1 = drop_features(house_train, features_to_drop)
+house_train1.info()
+
+cat_features = get_categorical_columns(house_train1)
+cont_features = get_continuous_columns(house_train1)
+print(cat_features)
+print(cont_features)
+
+house_train1 = transform_cat_to_cont(house_train1, cat_features, cont_features)
+rf = plot_feature_importances(house_train1, house_train['SalePrice'])
+fs_model = feature_selection.SelectFromModel(rf, prefit=True)
+house_train2 = fs_model.transform(house_train1)
+
+#explore sale price
 sns.kdeplot(house_train['SalePrice'])
 house_train['log_sale_price'] = np.log(house_train['SalePrice'])
 sns.kdeplot(house_train['log_sale_price'])
 
+#explore mszoing
 sns.countplot('MSZoning', data = house_train)
 sns.FacetGrid(house_train, hue="MSZoning",size=8).map(sns.kdeplot, "SalePrice").add_legend()
 sns.FacetGrid(house_train, row="MSZoning",size=8).map(sns.kdeplot, "SalePrice").add_legend()
 
+#explore neighborhood
+sns.countplot('Neighborhood', data = house_train).set_xticklabels(rotation=90)
+sns.FacetGrid(house_train, hue="Neighborhood",size=8).map(sns.kdeplot, "SalePrice").add_legend()
 
-#explore cabin column
-sns.countplot('Cabin', data = titanic_train)
-sns.FacetGrid(titanic_train, hue="Survived",size=8).map(sns.kdeplot, "Fare").add_legend()
+#explore OverallQual column
+sns.countplot('OverallQual', data = house_train)
+sns.FacetGrid(house_train, hue="OverallQual",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+sns.jointplot(x="OverallQual", y="SalePrice", data=house_train)
 
-#explore ticket column
-sns.countplot('Ticket', data = titanic_train)
-sns.factorplot(x='Ticket', hue='Survived', data=titanic_train,  kind="count", size=6).set_xticklabels(rotation=90)
 
-# view the median Age by the grouped features 
-grouped = titanic_train.groupby(['Sex','Pclass', 'Title'])  
-grouped.Age.median()
-titanic_train['Age'] = grouped.Age.apply(lambda x: x.fillna(x.median()))
-sns.FacetGrid(titanic_train, hue="Survived",size=8).map(sns.kdeplot, "Age").add_legend()
+#explore ordered categorical features
+sns.countplot('ExterQual', data = house_train)
+sns.FacetGrid(house_train, hue="ExterQual",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+sns.countplot('GarageQual', data = house_train)
+sns.FacetGrid(house_train, hue="GarageQual",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+ordinal_features1 = ["ExterQual", "ExterCond", "BsmtQual", "BsmtCond", "GarageQual", "GarageCond", "PoolQC", "FireplaceQu", "KitchenQual", "HeatingQC"]
+quality_dict1 = {None: 0, "Po": 1, "Fa": 2, "TA": 3, "Gd": 4, "Ex": 5 }
+transform_cat_to_cont(house_train, ordinal_features1, quality_dict1)
+sns.jointplot(x="ExterQual", y="SalePrice", data=house_train)
 
-#impute missing values for continuous features
-imputable_cont_features = ['Fare']
-cont_imputer = preprocessing.Imputer()
-cont_imputer.fit(titanic_train[imputable_cont_features])
-print(cont_imputer.statistics_)
-titanic_train[imputable_cont_features] = cont_imputer.transform(titanic_train[imputable_cont_features])
 
-#impute missing values for categorical features
-cat_imputer = CategoricalImputer()
-cat_imputer.fit(titanic_train['Embarked'])
-print(cat_imputer.fill_)
-titanic_train['Embarked'] = cat_imputer.transform(titanic_train['Embarked'])
+sns.countplot('BsmtExposure', data = house_train)
+sns.FacetGrid(house_train, hue="BsmtExposure",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+ordinal_features2 = ["BsmtExposure"]
+quality_dict2 = {None: 0, "No": 1, "Mn": 2, "Av": 3, "Gd": 4 }
+transform_cat_to_cont(house_train, ordinal_features2, quality_dict2)
 
-#size of families (including the passenger)
-sns.FacetGrid(titanic_train, hue="Survived",size=8).map(sns.kdeplot, "FamilySize").add_legend()
+#explore central air conditioning
+sns.countplot('CentralAir', data = house_train)
+sns.FacetGrid(house_train, hue="CentralAir",size=8).map(sns.kdeplot, "SalePrice").add_legend()
 
-cat_features = ['Sex', 'Embarked', 'Pclass', 'Cabin', 'Title', 'Ticket']
-cont_features = ['Age', 'Fare', 'SibSp', 'Parch', 'FamilySize']
+#explore 1stfloor sqft
+sns.kdeplot(house_train['1stFlrSF'])
+sns.jointplot(x="1stFlrSF", y="SalePrice", data=house_train)
 
-feature_defs = []
-for col_name in cat_features:
-    feature_defs.append((col_name, MyLabelBinarizer()))
-    
-for col_name in cont_features:
-    feature_defs.append((col_name, None))
+#explore bedroom feature
+sns.countplot('BedroomAbvGr', data = house_train)
+sns.FacetGrid(house_train, hue="BedroomAbvGr",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+sns.jointplot(x="BedroomAbvGr", y="SalePrice", data=house_train)
 
-mapper = DataFrameMapper(feature_defs, input_df=True, df_out=True)
-mapper.fit(titanic_train)
-X_train = mapper.transform(titanic_train)
-y_train = titanic_train['Survived']
+#explore garage year built
+sns.countplot('YearBuilt', data = house_train)
+sns.FacetGrid(house_train, hue="YearBuilt",size=8).map(sns.kdeplot, "SalePrice").add_legend()
+sns.jointplot(x="YearBuilt", y="SalePrice", data=house_train)
 
-kfold = model_selection.StratifiedKFold(n_splits=10)
-rf_classifier = ensemble.RandomForestClassifier(random_state=100, n_jobs=1, verbose=1, oob_score=True)
-rf_grid = {'max_depth':list(range(7,14)), 'n_estimators':list(range(10,100,10)),  
-           'min_samples_split':list(range(4,11)), 'min_samples_leaf':list(range(2,5))}
-rf_grid_classifier = model_selection.GridSearchCV(rf_classifier, rf_grid, cv=kfold, refit=True, return_train_score=True)
-rf_grid_classifier.fit(X_train, y_train)
 
-#check overfitting due to parameters(i.e.,capacity of the model)
-results = rf_grid_classifier.cv_results_
-print(results.get('mean_test_score'))
-print(results.get('mean_train_score'))
-print(rf_grid_classifier.best_params_)
-print(rf_grid_classifier.best_score_)
-final_model = rf_grid_classifier.best_estimator_
-
-#check overfitting due to size of the data
-plot_learning_curve(final_model,"RF learning curves",X_train,y_train,cv=kfold)
-
-#plot the feature importances
-plot_feature_importances(final_model, X_train, "Random Forest")
-
-#persist the model
-joblib.dump(final_model, 'final_model.pkl')
-
-titanic_test = titanic_all[titanic_train.shape[0]:]
-titanic_test['Age'] = grouped.Age.apply(lambda x: x.fillna(x.median()))
-titanic_test[imputable_cont_features] = cont_imputer.transform(titanic_test[imputable_cont_features])
-titanic_test['Embarked'] = cat_imputer.transform(titanic_test['Embarked'])
-X_test = mapper.transform(titanic_test)
-
-titanic_test['Survived'] = final_model.predict(X_test)
-titanic_test.to_csv(os.path.join(path,'submission.csv'), columns=['PassengerId','Survived'], index=False)
+house_test.to_csv(os.path.join(path,'submission.csv'), columns=['PassengerId','Survived'], index=False)
