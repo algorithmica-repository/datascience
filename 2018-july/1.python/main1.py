6ac2c1caad029d12a998eba06a107d37f5882947
Algorithmica <info@algorithmica.co.in>
2018-07-21 11:04:01 +0530

    Add files via upload

---
+++ 2018-july/1.python/main1.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    return a+b
+
+print(__name__)
+if __name__ == '__main__':
+    print(add(10, 20))
+    print(add(20,30))
---
+++ 2018-july/1.python/main2.py
@@ -0,0 +1,4 @@
+import main1
+
+print(main1.__name__)
+print(main1.add(10, 20))
