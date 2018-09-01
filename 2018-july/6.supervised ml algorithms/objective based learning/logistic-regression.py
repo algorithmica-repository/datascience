c81f04db94f8a6415d7ebe96592516e6fb63c30b
Algorithmica <info@algorithmica.co.in>
2018-09-01 10:08:30 +0530

    Add files via upload

---
+++ 2018-july/6.supervised ml algorithms/objective based learning/logistic-regression.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn import linear_model, svm, preprocessing, model_selection
+from sklearn_pandas import CategoricalImputer
+
+       
+titanic_train = pd.read_csv("C:/Users/Algorithmica/Downloads/all/train.csv")
+print(titanic_train.shape)
+print(titanic_train.info())
+
+imputable_cont_features = ['Age','Fare']
+cont_imputer = preprocessing.Imputer()
+cont_imputer.fit(titanic_train[imputable_cont_features])
+print(cont_imputer.statistics_)
+titanic_train[imputable_cont_features] = cont_imputer.transform(titanic_train[imputable_cont_features])
+
+#impute missing values for categorical features
+cat_imputer = CategoricalImputer()
+cat_imputer.fit(titanic_train['Embarked'])
+print(cat_imputer.fill_)
+titanic_train['Embarked'] = cat_imputer.transform(titanic_train['Embarked'])
+
+#creaate categorical age column from age
+def convert_age(age):
+    if(age >= 0 and age <= 18): 
+        return 'Teen'
+    elif(age <= 40): 
+        return 'Young'
+    elif(age <= 60): 
+        return 'Middle'
+    else: 
+        return 'Old'
+titanic_train['Age1'] = titanic_train['Age'].map(convert_age)
+
+titanic_train['FamilySize'] = titanic_train['SibSp'] +  titanic_train['Parch'] + 1
+
+def convert_familysize(size):
+    if(size == 1): 
+        return 'Single'
+    elif(size <=5): 
+        return 'Medium'
+    else: 
+        return 'Large'
+titanic_train['FamilySize1'] = titanic_train['FamilySize'].map(convert_familysize)
+     
+def extract_title(name):
+     return name.split(',')[1].split('.')[0].strip()
+titanic_train['Title'] = titanic_train['Name'].map(extract_title)
+    
+cat_columns = ['Sex', 'Embarked', 'Pclass', 'Title', 'Age1', 'FamilySize1']
+titanic_train1 = pd.get_dummies(titanic_train, columns = cat_columns)
+titanic_train1.drop(['PassengerId', 'Name', 'Ticket', 'Cabin', 'Survived'], axis=1, inplace=True)
+
+X_train = titanic_train1
+y_train = titanic_train['Survived']
+
+lr_estimator = linear_model.LogisticRegression(random_state=100)
+lr_grid = {'C':[0.1, 0.5, 1], 'penalty':['l1','l2'], 'max_iter':[100,200,300]}
+grid_lr_estimator = model_selection.GridSearchCV(lr_estimator, lr_grid, cv=10, return_train_score=True)
+grid_lr_estimator.fit(X_train, y_train)
+best_est = grid_lr_estimator.best_estimator_
+print(grid_lr_estimator.best_score_)
+print(grid_lr_estimator.best_params_)
+print(grid_lr_estimator.cv_results_.get('mean_train_score'))
+print(grid_lr_estimator.cv_results_.get('mean_test_score'))
+print(best_est.coef_)
+print(best_est.intercept_)
