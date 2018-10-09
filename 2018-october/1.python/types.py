0bfbea6cbcba5d1491791c7e89880b02445c2092
Algorithmica <info@algorithmica.co.in>
2018-10-09 09:59:41 +0530

    Add files via upload

---
+++ 2018-october/1.python/types.py
@@ -0,0 +1,16 @@
+a = 10
+print(a)
+
+b = 10.6
+print(b)
+
+c = 'abc'
+print(c)
+
+d = True
+print(d)
+
+#types are inferred automatically in python
+#how do you get inferred type of a variable?
+print(type(a))
+print(type(d))
