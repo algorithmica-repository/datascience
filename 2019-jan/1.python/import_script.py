4a4aa7d5d08792626122c868afb10c3991798286
Algorithmica <info@algorithmica.co.in>
2019-02-27 21:09:05 +0530

    Add files via upload

---
+++ 2019-jan/1.python/import_script.py
@@ -0,0 +1,4 @@
+import main_in_python as dummy
+
+print(dummy.__name__)
+print(dummy.add(10, 20))
---
+++ 2019-jan/1.python/main_in_python.py
@@ -0,0 +1,7 @@
+def add(x,y):
+    return x+y
+
+print(__name__)
+
+if __name__ == '__main__':
+    print(add(10,30) )
