8ba80663fb12d332ca210bdfb9de274ecfa416a0
Algorithmica <info@algorithmica.co.in>
2019-04-20 07:54:08 +0530

    Add files via upload

--- 2019-jan/13.outlier detection/outlier detection.py
+++ 2019-jan/13.outlier detection/outlier detection.py
@@ -18,20 +18,20 @@ import pandas as pd
 import numpy as np
 
 outliers_fraction = 0.25
-cluster_separation = 0
+cluster_separation = 2
 n_samples = 200
 X, y = generate_synthetic_data_outliers(n_samples, outliers_fraction, cluster_separation)
 plot_data_2d_classification(X, y, xlim=[-7,7], ylim=[-7,7])
 
 iso_forest_estimator = ensemble.IsolationForest()
 iso_forest_grid = {'contamination':[0.1, 0.2, 0.25, 0.3]}
-grid_search_plot_models_classification(iso_forest_estimator, iso_forest_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True, levels=[0])
+grid_search_plot_models_classification(iso_forest_estimator, iso_forest_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True)
 
 cov_estimator = covariance.EllipticEnvelope()
 cov_grid = {'contamination':[0.1, 0.2, 0.25, 0.3]}
-grid_search_plot_models_classification(cov_estimator, cov_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True, levels=[0])
+grid_search_plot_models_classification(cov_estimator, cov_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True)
 
 svm_estimator = svm.OneClassSVM(kernel="rbf", gamma=0.1)
 tmp = 0.95 * outliers_fraction
 svm_grid = {'nu':[tmp+0.03, tmp+0.05, tmp+0.06, tmp+0.07]}
-grid_search_plot_models_classification(svm_estimator, svm_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True, levels=[0])
+grid_search_plot_models_classification(svm_estimator, svm_grid, X, y, xlim=[-7,7], ylim=[-7,7], outlier_estimator=True)
