c29e988ae7410bd812332c743c3e34987fce2001
Algorithmica <info@algorithmica.co.in>
2019-05-04 22:07:10 +0530

    Update 7.bagged ensemble regression.py

--- 2019-jan/11.regression/7.bagged ensemble regression.py
+++ 2019-jan/11.regression/7.bagged ensemble regression.py
@@ -16,16 +16,16 @@ scoring = metrics.make_scorer(rmse, greater_is_better=False)
 
 rf_estimator = ensemble.RandomForestRegressor()
 rf_grid = {'n_estimators':list(range(10,100,40)), 'max_depth':list(range(3,6))}
+grid_search_plot_models_2d_regression(rf_estimator, rf_grid, X_train, y_train )
 grid_search_plot_two_parameter_curves(rf_estimator, rf_grid, X_train, y_train, scoring =  scoring)
-grid_search_plot_models_regression(rf_estimator, rf_grid, X_train, y_train )
 rf_final_model = grid_search_best_model(rf_estimator, rf_grid, X_train, y_train, scoring = scoring )
 plot_model_2d_regression(rf_final_model, X_train, y_train)
 regression_performance(rf_final_model, X_test, y_test)
 
 et_estimator = ensemble.ExtraTreesRegressor()
 et_grid = {'n_estimators':list(range(10,100,40)), 'max_depth':list(range(3,6))}
+grid_search_plot_models_2d_regression(et_estimator, et_grid, X_train, y_train )
 grid_search_plot_two_parameter_curves(et_estimator, et_grid, X_train, y_train, scoring =  scoring)
-grid_search_plot_models_regression(et_estimator, et_grid, X_train, y_train )
 et_final_model = grid_search_best_model(et_estimator, et_grid, X_train, y_train, scoring = scoring )
 plot_model_2d_regression(et_final_model, X_train, y_train)
 regression_performance(et_final_model, X_test, y_test)
@@ -37,6 +37,7 @@ plot_data_3d_regression(X_train, y_train)
 
 rf_estimator = ensemble.RandomForestRegressor()
 rf_grid = {'n_estimators':list(range(10,100,40)), 'max_depth':list(range(3,6))}
+grid_search_plot_models_3d_regression(rf_estimator, rf_grid, X_train, y_train )
 grid_search_plot_two_parameter_curves(rf_estimator, rf_grid, X_train, y_train, scoring =  scoring)
 et_final_model = grid_search_best_model(rf_estimator, rf_grid, X_train, y_train, scoring = scoring )
 plot_model_3d_regression(et_final_model, X_train, y_train)
@@ -44,6 +45,7 @@ regression_performance(et_final_model, X_test, y_test)
 
 et_estimator = ensemble.ExtraTreesRegressor()
 et_grid = {'n_estimators':list(range(10,100,40)), 'max_depth':list(range(3,6))}
+grid_search_plot_models_3d_regression(et_estimator, et_grid, X_train, y_train )
 grid_search_plot_two_parameter_curves(et_estimator, et_grid, X_train, y_train, scoring =  scoring)
 et_final_model = grid_search_best_model(et_estimator, et_grid, X_train, y_train, scoring = scoring )
 plot_model_3d_regression(et_final_model, X_train, y_train)
