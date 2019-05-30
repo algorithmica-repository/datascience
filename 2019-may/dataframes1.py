a1a0efbfe8a70d526876641f5fe121acca210b49
Algorithmica <info@algorithmica.co.in>
2019-05-30 09:47:27 +0530

    Add files via upload

--- 2019-may/dataframes1.py
+++ 2019-may/dataframes1.py
@@ -7,14 +7,22 @@ persons = {'name':['abc','def','xyz'],
            'fare':[12.5, 40.3, 50]
            }
 df1 = pd.DataFrame(persons)
+print(df1)
 print(df1.shape)
 print(df1.info())
 
+name =['abc','def','xyz']
+age = [30, 25, 15]
+fare = [12.5, 40.3, 50]
+df1 = pd.DataFrame({'name': name, 'age':age, 'fare':fare})
 print(df1)
+print(df1.shape)
+print(df1.info())
+
 print(df1.index)
 print(df1.values)
-df1.index = df1.name
-df1 = df1.reset_index()
+df1 = df1.set_index(df1.age)
+df1 = df1.reset_index(drop=True)
 
 
 titanic_train = pd.read_csv('C:/Users/Algorithmica/Downloads/titanic_train.csv')
