07849db9a355ff9a3c2f5c1ea02107bfe1898366
Algorithmica <info@algorithmica.co.in>
2019-06-05 18:20:36 +0530

    Add files via upload

---
+++ 2019-may/1.python/3.tuple.py
@@ -0,0 +1,19 @@
+#tuple: An immutable indexed container of data
+t1 = (10, 20, 30.5, True, 'abc')
+print(t1)
+print(type(t1))
+
+print(t1[1])
+print(t1[:3])
+print(t1[-1])
+
+t1[0] = 60
+
+t2 = (100, t1)
+print(t2)
+
+for x in t1:
+    print(x)
+    
+for i,x in enumerate(t1):
+print(i, x)
