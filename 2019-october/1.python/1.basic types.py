d4ec491a8c1efab4ebff5553bc73ef7c398830b7
Algorithmica <info@algorithmica.co.in>
2019-10-13 20:07:47 +0530

    Add files via upload

---
+++ 2019-october/1.python/1.basic types.py
@@ -0,0 +1,15 @@
+a = 10
+print(type(a))
+print(a)
+
+b = 20.6
+print(type(b))
+print(b)
+
+c = True
+print(type(c))
+print(c)
+
+d = "abc"
+print(type(d))
+print(d)
---
+++ 2019-october/1.python/2.list.py
@@ -0,0 +1,34 @@
+list1 = [10, 20, 'abc', True, 30.6]
+print(type(list1))
+print(list1)
+print(id(list1))
+
+list2 = ['xyz', 'a', "abc", 12]
+print(list2)
+print(id(list2))
+
+list3 = list2
+print(list3)
+print(id(list3))
+
+list4 = list2.copy()
+print(list4)
+print(id(list4))
+
+print(list1 == list2)
+print(list2 == list3)
+
+list5 = [10, list1, 'abc', True, list4]
+print(list5)
+print(type(list5))
+
+#element access
+list1[2]
+list1[1:4]
+list1[1:4:2]
+list1[2:]
+list1[:3]
+
+#manipulate list
+list1.append(50)
+list1.pop()
