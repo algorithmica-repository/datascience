5c5396f56abf90319cd9bff5991be283ba8fa127
Algorithmica <info@algorithmica.co.in>
2019-10-22 09:57:57 +0530

    Add files via upload

---
+++ 2019-october/4.model evaluation/model evaluation2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+from sklearn import neighbors, model_selection
+
+dir = 'E:/'
+titanic_train = pd.read_csv(os.path.join(dir, 'train.csv'))
+print(titanic_train.info())
+print(titanic_train.columns)
+
+X_train = titanic_train[ ['SibSp', 'Parch'] ]
+y_train = titanic_train['Survived']
+knn_estimator = neighbors.KNeighborsClassifier()
+knn_estimator.fit(X_train, y_train)
+model_selection.cross_val_score(knn_estimator, X_train, y_train, scoring="accuracy", cv=5).mean()
+
+titanic_test = pd.read_csv(os.path.join(dir, 'test.csv'))
+print(titanic_test.info())
+X_test = titanic_test[ ['SibSp', 'Parch'] ]
+titanic_test['Survived'] = knn_estimator.predict(X_test)
+titanic_test.to_csv(os.path.join(dir, 'submission.csv'), columns=['PassengerId', 'Survived'], index=False)
