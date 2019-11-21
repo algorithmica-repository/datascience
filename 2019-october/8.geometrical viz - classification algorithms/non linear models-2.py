585c8635e719d4b58aa898868834233c9bbba4d4
Algorithmica <info@algorithmica.co.in>
2019-11-21 13:49:54 +0530

    Add files via upload

--- 2019-october/8.geometrical viz - classification algorithms/non linear models-2.py
+++ 2019-october/8.geometrical viz - classification algorithms/non linear models-2.py
@@ -1,8 +1,9 @@
 import sys
 sys.path.append("E:/New Folder/utils")
 
+import kernel_utils as kutils
 import classification_utils as cutils
-from sklearn import model_selection, linear_model, svm, preprocessing, pipeline
+from sklearn import model_selection, linear_model, svm, preprocessing, pipeline, neural_network
 
 
 #2-d classification pattern
@@ -15,12 +16,11 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 cutils.plot_data_2d_classification(X_train, y_train)
 
 #perceptron algorithm
-stages = [
-            ('features', preprocessing.PolynomialFeatures()),
-            ('clf', linear_model.Perceptron(max_iter=1000))
-        ]
+stages = [('features', kutils.KernelTransformer('rbf')) ,
+          ('clf', linear_model.Perceptron(max_iter=1000))
+          ]
 perceptron_pipeline = pipeline.Pipeline(stages)
-perceptron_pipeline_grid  = {'clf__penalty':['l1'], 'clf__alpha':[0, 0.1, 0.3, 0.5], 'features__degree':[2,3,5,10]}
+perceptron_pipeline_grid  = {'features__gamma':[0.1, 0.01, 0.2]}
 pipeline_object = cutils.grid_search_best_model(perceptron_pipeline, perceptron_pipeline_grid, X_train, y_train)
 final_estimator = pipeline_object.named_steps['clf']
 print(final_estimator.intercept_)
@@ -28,27 +28,28 @@ print(final_estimator.coef_)
 cutils.plot_model_2d_classification(pipeline_object, X_train, y_train)
 
 #logistic regression algorithm
-stages = [
-            ('features', preprocessing.PolynomialFeatures()),
-            ('clf', linear_model.LogisticRegression())
-        ]
+stages = [('features', kutils.KernelTransformer('rbf')) ,
+          ('clf', linear_model.LogisticRegression())
+          ]
+
 lr_pipeline = pipeline.Pipeline(stages)
-lr_pipeline_grid  = {'clf__penalty':['l1'], 'clf__C':[0.01, 0.1, 0.3, 0.5], 'features__degree':[2,3,5,10]}
+lr_pipeline_grid  = {'features__gamma':[0.1, 1, 5,10]}
 pipeline_object = cutils.grid_search_best_model(lr_pipeline, lr_pipeline_grid, X_train, y_train)
 final_estimator = pipeline_object.named_steps['clf']
 print(final_estimator.intercept_)
-print(final_estimator.coef_)
 cutils.plot_model_2d_classification(pipeline_object, X_train, y_train)
 
 #linear svm algorithm
-stages = [
-            ('features', preprocessing.PolynomialFeatures()),
-            ('clf', svm.LinearSVC())
-        ]
-svm_pipeline = pipeline.Pipeline(stages)
-svm_pipeline_grid  = {'clf__penalty':['l2'], 'clf__C':[0.01, 0.1, 0.3, 0.5], 'features__degree':[2,3,5,10, 50, 100]}
-pipeline_object = cutils.grid_search_best_model(svm_pipeline, svm_pipeline_grid, X_train, y_train)
-final_estimator = pipeline_object.named_steps['clf']
+kernel_svm_estimator = svm.SVC(kernel='rbf')
+kernel_svm_grid = {'gamma':[0.01, 0.1, 1, 2, 5, 10] }
+final_estimator = cutils.grid_search_best_model(kernel_svm_estimator, kernel_svm_grid, X_train, y_train)
 print(final_estimator.intercept_)
-print(final_estimator.coef_)
-cutils.plot_model_2d_classification(pipeline_object, X_train, y_train)
+cutils.plot_model_2d_classification(final_estimator, X_train, y_train)
+
+#artifical neural network
+ann_estimator = neural_network.MLPClassifier()
+ann_grid = {'hidden_layer_sizes':[(3, 4), (10, 20)] }
+final_estimator = cutils.grid_search_best_model(ann_estimator, ann_grid, X_train, y_train)
+print(final_estimator.intercepts_)
+print(final_estimator.coefs_)
+cutils.plot_model_2d_classification(final_estimator, X_train, y_train)
